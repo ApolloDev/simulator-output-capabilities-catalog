d73c5834fb86c72e0b7d3b5a706bebfb6bdae905
JohnLevander <jdl50@pitt.edu>
2016-06-02 16:49:58 -0400

    Created the template file

---
+++ src/main/java/edu/pitt/apollo/types/SimulatorCapabilities.java
@@ -0,0 +1,14 @@
+package edu.pitt.apollo.types;
+
+import java.util.List;
+
+/**
+ * Created by jdl50 on 6/1/16.
+ */
+public class SimulatorCapabilities {
+
+    private List<String> supportedSpecies;
+
+
+
+}
